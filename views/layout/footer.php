89a0e0598fbd134b11690bd8831f98e9bc3860fd
eivancode <ivanalvarez78@aragon.unam.mx>
2023-02-17 16:24:42 -0600

    Se añaden correcciones

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -10,8 +10,7 @@
   <script src="<?= base_url ?>plugins/jquery/jquery.min.js"></script>
   <!-- AdminLTE -->
   <script src="<?= base_url ?>js/adminlte.min.js"></script>
-  <!-- App -->
-  <script src="<?= base_url ?>js/app.js"></script>
+  <!-- App --> 
   <script src="<?= base_url ?>bootstrap/bootstrap.min.js"></script>
   <!-- Plugins -->
   <script src="<?= base_url ?>plugins/sweetalert2/sweetalert2.all.min.js"></script>
